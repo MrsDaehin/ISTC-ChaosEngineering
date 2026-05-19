7a1b718b15334d477867d923a23d44111d1e0b9a
Almudena Vivanco <almudena.vivanco@gmail.com>
2026-05-19 16:54:20 +0200

    added containerd

--- ChaosMesh/04_helm_install_chaos_mesh.sh
+++ ChaosMesh/04_helm_install_chaos_mesh.sh
@@ -5,3 +5,15 @@ VERSION=2.7.3 && helm install chaos-mesh chaos-mesh/chaos-mesh \
   --set chaosDaemon.env.DOCKER_API_VERSION="1.40" \
   --set dashboard.securityMode=false \
   --set dashboard.service.nodePort=31111
+
+
+  # with containerd for killercoda
+
+VERSION=2.7.3 && helm install chaos-mesh chaos-mesh/chaos-mesh \
+  --version $VERSION \
+  --namespace chaos-testing \
+  --create-namespace \
+  --set chaosDaemon.runtime=containerd \
+  --set chaosDaemon.socketPath=/run/containerd/containerd.sock \
+  --set dashboard.securityMode=false \
+  --set dashboard.service.nodePort=31111
